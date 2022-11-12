9a2d62d7a0040945143d61af17fa2e5c0a5d8254
Jarrod Mishima <mishimajam@gmail.com>
2022-11-12 15:45:55 -0800

    fixed watchliststocks seeder and successfully migrated

--- app/seeds/watchlistStocks.py
+++ app/seeds/watchlistStocks.py
@@ -25,7 +25,7 @@ def seed_watchlist_stocks():
 # incrementing primary key, CASCADE deletes any dependent entities.  With
 # sqlite3 in development you need to instead use DELETE to remove all data and
 # it will reset the primary keys for you as well.
-def undo_watchlist():
+def undo_watchlist_stocks():
     if environment == "production":
         db.session.execute(f"TRUNCATE table {SCHEMA}.watchlist_stocks RESTART IDENTITY CASCADE;")
     else:
