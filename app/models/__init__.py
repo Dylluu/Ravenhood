03a192166bc54b23004b6d40b0e1fc1461698668
Jarrod Mishima <mishimajam@gmail.com>
2022-11-11 16:09:49 -0800

    model + seeder for db

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -1,3 +1,4 @@
 from .db import db
 from .user import User
+from .watchlist import Watchlist
 from .db import environment, SCHEMA
--- app/models/watchlist.py
+++ app/models/watchlist.py
@@ -1,10 +1,21 @@
 from .db import db, environment, SCHEMA, add_prefix_for_prod
 
 class Watchlist(db.Model):
-   __tablename__= 'watchlists'
+
+  __tablename__= 'watchlists'
 
   if environment == "production":
-        __table_args__ = {'schema': SCHEMA}
+    __table_args__={'schema': SCHEMA}
 
 
   id = db.Column(db.Integer, primary_key=True)
+  name = db.Column(db.String(40), nullable=False)
+  user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
+  # not sure if its user/users
+
+  def to_dict(self):
+    return {
+      'id': self.id,
+      'name': self.name,
+      'user_id': self.user_id
+    }
