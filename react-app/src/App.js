f133eab918d15039b5c38806f652a88dced110ec
Dylan Luu <dylanluu@ucsb.edu>
2022-11-12 17:43:13 -0500

    Started signup form

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -38,7 +38,7 @@ function App() {
         <Route path='/login' exact={true}>
           <RealLoginForm />
         </Route>
-        <Route path='/sign-up' exact={true}>
+        <Route path='/signup' exact={true}>
           <SignUpForm />
         </Route>
         <ProtectedRoute path='/users' exact={true} >
