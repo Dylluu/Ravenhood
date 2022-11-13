58770a8bb1b02a3f9053ac1a6adbecd4e86a157d
Jarrod Mishima <mishimajam@gmail.com>
2022-11-12 17:26:01 -0800

    watchlist actions made for getone/post/update/delete

--- react-app/src/store/watchlist.js
+++ react-app/src/store/watchlist.js
@@ -1,16 +1,45 @@
-const GET_WATCHLIST = 'watchlist/GET_WATCHLIST';
+const GET_ALL_WATCHLIST = 'watchlist/GET_ALL_WATCHLIST';
+const GET_ONE_WATCHLIST = 'watchlist/GET_ONE_WATCHLIST';
+const POST_WATCHLIST = 'watchlist/POST_WATCHLIST';
+const UPDATE_WATCHLIST = 'watchlist/UPDATE_WATCHLIST';
+const DELETE_WATCHLIST = 'watchlist/DELETE_WATCHLIST';
 
-const getWatchlist = (watchlist) => ({
-  type: GET_WATCHLIST,
+const getAllWatchlist = (watchlist) => ({
+  type: GET_ALL_WATCHLIST,
   watchlist
 });
+
+const getOneWatchlist = (watchlist) => ({
+  type: GET_ONE_WATCHLIST,
+  watchlist
+})
+
+const postWatchlist = (watchlist) => ({
+  type: POST_WATCHLIST,
+  watchlist
+})
+
+const updateWatchlist = (watchlist) => ({
+  type: UPDATE_WATCHLIST,
+  watchlist
+})
+
+const deleteWatchlist = (watchlist) => ({
+  type: DELETE_WATCHLIST,
+  watchlist
+})
+
+
 
 const initialState = { watchlist: null}
 
 export default function watchlist(state = initialState, action) {
   const newState = {...state};
   switch (action.type) {
-    case GET_WATCHLIST:
+    case GET_ALL_WATCHLIST:
+      newState.watchlist = action.watchlist;
+      return newState;
+    case GET_ONE_WATCHLIST:
       newState.watchlist = action.watchlist;
       return newState;
     default:
