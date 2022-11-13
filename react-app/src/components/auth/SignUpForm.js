8a6fd1bdc6bc8f9c54e8faed06e5533b67c938a5
Dylan Luu <dylanluu@ucsb.edu>
2022-11-12 20:28:00 -0500

    Done with signup main inputs

--- react-app/src/components/auth/SignUpForm.js
+++ react-app/src/components/auth/SignUpForm.js
@@ -4,28 +4,33 @@ import { Redirect, NavLink } from 'react-router-dom';
 import { signUp } from '../../store/session';
 import './SignUpForm.css';
 import robinHoodLogo from '../../assets/Robinhood-logo.png';
+import stockBlocks from '../../assets/stockBlocksGreen.png';
 
 const SignUpForm = () => {
   const [errors, setErrors] = useState([]);
-  const [username, setUsername] = useState('');
+  const [firstName, setFirstName] = useState('');
+  const [lastName, setLastName] = useState('');
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
   const [repeatPassword, setRepeatPassword] = useState('');
+  const [pwVisible, setPwVisible] = useState(false);
   const user = useSelector(state => state.session.user);
   const dispatch = useDispatch();
 
   const onSignUp = async (e) => {
     e.preventDefault();
-    if (password === repeatPassword) {
-      const data = await dispatch(signUp(username, email, password));
+      const data = await dispatch(signUp(firstName, lastName, email, password));
       if (data) {
         setErrors(data)
       }
-    }
   };
 
-  const updateUsername = (e) => {
-    setUsername(e.target.value);
+  const updateFirstName = (e) => {
+    setFirstName(e.target.value);
+  };
+
+  const updateLastName = (e) => {
+    setLastName(e.target.value);
   };
 
   const updateEmail = (e) => {
@@ -44,57 +49,116 @@ const SignUpForm = () => {
     return <Redirect to='/' />;
   }
 
+  function handlePasswordIcon() {
+    if(!pwVisible){
+        const openEye = document.getElementById('signup-open-eye');
+        openEye.style.display = 'none'
+        openEye.style.zIndex = '-1'
+        const closedEye = document.getElementById('signup-closed-eye');
+        closedEye.style.display = 'flex'
+        closedEye.style.zIndex = '1'
+        const pwInput = document.getElementById('signup-form-password');
+        pwInput.setAttribute('type', '')
+        setPwVisible(true)
+    }
+    if(pwVisible){
+        const openEye = document.getElementById('signup-open-eye');
+        openEye.style.display = 'flex'
+        openEye.style.zIndex = '1'
+        const closedEye = document.getElementById('signup-closed-eye');
+        closedEye.style.display = 'none'
+        closedEye.style.zIndex = '-1'
+        const pwInput = document.getElementById('signup-form-password');
+        pwInput.setAttribute('type', 'password')
+        setPwVisible(false)
+    }
+}
+
+const handlePasswordBorder = () => {
+  const passwordDiv = document.getElementsByClassName('signup-form-password-input');
+  passwordDiv[0].classList.add('signup-form-password-input-focus');
+  passwordDiv[0].removeAttribute('id')
+  const passwordInput = document.getElementById('signup-form-password');
+  passwordInput.addEventListener('focusout', removePasswordBorder)
+}
+function removePasswordBorder() {
+  const passwordDiv = document.getElementsByClassName('signup-form-password-input');
+  passwordDiv[0].classList.remove('signup-form-password-input-focus');
+  passwordDiv[0].setAttribute('id', 'signup-form-password-input')
+  // console.log('in remove')
+}
+
   return (
     <div className='signup-form-wrapper'>
       <div className='signup-form-left'>
         <NavLink to='/'><img alt='robinHoodLogo' src={robinHoodLogo} id='robinHoodLogo'/></NavLink>
         <p className='signup-form-left-text'>Get started with free stock on us</p>
+        <img id='signup-form-stockblocks' alt='stockBlocks' src={stockBlocks}/>
       </div>
+      <div className='signup-form-right'>
+        <div className='signup-form-right-inner'>
     <form onSubmit={onSignUp}>
+      <div className='signup-form-right-header'>Enter your first and last name as they appear on your government ID.</div>
       <div>
         {errors.map((error, ind) => (
           <div key={ind}>{error}</div>
         ))}
       </div>
-      <div>
-        <label>User Name</label>
+      <div id='signup-first-and-last'>
         <input
           type='text'
-          name='username'
-          onChange={updateUsername}
-          value={username}
+          name='firstName'
+          onChange={updateFirstName}
+          value={firstName}
+          className='first-and-last-input'
+          placeholder='First name'
+        ></input>
+        <input
+          type='text'
+          name='lastName'
+          onChange={updateLastName}
+          value={lastName}
+          className='first-and-last-input'
+          placeholder='Last name'
         ></input>
       </div>
       <div>
-        <label>Email</label>
         <input
           type='text'
           name='email'
           onChange={updateEmail}
           value={email}
+          placeholder='Email address'
+          className='signup-form-inputs'
         ></input>
       </div>
-      <div>
-        <label>Password</label>
+      <div className='signup-form-password-input'
+      id='signup-form-password-input'
+      >
         <input
           type='password'
           name='password'
           onChange={updatePassword}
           value={password}
+          placeholder='Password (min. 10 characters)'
+          id='signup-form-password'
+          onClick={() => handlePasswordBorder()}
         ></input>
+        <div className='password-visibility'
+        onClick={() => handlePasswordIcon()}
+        >
+        <i className="fa-solid fa-eye"
+        id="signup-open-eye"
+        />
+        <i className="fa-solid fa-eye-slash"
+        id="signup-closed-eye"
+        />
+        </div>
       </div>
-      <div>
-        <label>Repeat Password</label>
-        <input
-          type='password'
-          name='repeat_password'
-          onChange={updateRepeatPassword}
-          value={repeatPassword}
-          required={true}
-        ></input>
-      </div>
-      <button type='submit'>Sign Up</button>
+      {/* <button type='submit'>Sign Up</button> */}
     </form>
+    </div>
+    </div>
     </div>
   );
 };
