0b90735671bd7476387742055f9a84c190b6ddc7
NhutLinh-Ngo <nngo6133@gmail.com>
2022-11-15 21:32:36 -0800

    add stock dash board

--- react-app/src/components/StockDashBoard/index.js
+++ react-app/src/components/StockDashBoard/index.js
@@ -1,19 +1,18 @@
 import React from 'react';
-
+import MainStockGraph from '../MainStockGraph';
+import './StockDashBoard.css';
 function StockDashBoard() {
 	return (
-		<div className="dashboard-wrapper">
-			<div className="dashboard-inner-wrapper">
-				<div className="dashboard-inner-left">
-					<div className="dashboard-graph-wrapper"></div>
+		<div className="stock-dashboard-wrapper">
+			<div className="stock-dashboard-inner-wrapper">
+				<div className="stock-dashboard-inner-left">
+					<div className="stock-dashboard-graph-wrapper">
+						<MainStockGraph />
+					</div>
 
-					<div className="dashboard-inner-left-borders"></div>
+					<div className="stock-dashboard-inner-left-borders"></div>
 				</div>
-				<div className="dashboard-watchlists-wrapper">
-					<div className="watchlists-header-dashboard"></div>
-
-					<div className="watchlist-list-body"></div>
-				</div>
+				<div className="stock-dashboard-buy-sell-wrapper"></div>
 			</div>
 		</div>
 	);
