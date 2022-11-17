2ef1af95cfe7a13f20d7b51f7fb4e46814150674
NhutLinh-Ngo <nngo6133@gmail.com>
2022-11-16 20:50:25 -0800

    add PortfolioGraph function

---
+++ react-app/src/components/PortfolioGraph/index.js
@@ -0,0 +1,9 @@
+import { useEffect, useState, useMemo } from 'react';
+import { useParams } from 'react-router-dom';
+import Chart from 'react-apexcharts';
+import { useDispatch, useSelector } from 'react-redux';
+import './PortfolioGraph.css';
+
+function PortfolioGraph() {}
+
+export default PortfolioGraph;
