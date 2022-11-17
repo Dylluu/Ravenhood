b12a05a4cdb92f8f98f6005367fab524e152fc13
Dylan Luu <dylanluu@ucsb.edu>
2022-11-17 01:45:35 -0500

    Ready to test login and signup dynamic error validations

--- app/forms/signup_form.py
+++ app/forms/signup_form.py
@@ -1,6 +1,6 @@
 from flask_wtf import FlaskForm
 from wtforms import SelectField, StringField, SubmitField, BooleanField, IntegerField, TextAreaField
-from wtforms.validators import DataRequired, Email, ValidationError
+from wtforms.validators import DataRequired, Email, ValidationError, Length
 from app.models import User
 
 
@@ -8,6 +8,8 @@ def user_exists(form, field):
     # Checking if user exists
     email = field.data
     user = User.query.filter(User.email == email).first()
+    if not '@' in email or not '.' in email:
+        raise ValidationError('Please enter a valid email address.')
     if user:
         raise ValidationError('Email address is already in use.')
 
@@ -22,8 +24,8 @@ def user_exists(form, field):
 v = [DataRequired()]
 class SignUpForm(FlaskForm):
     first_name = StringField(
-        'first_name', validators=[DataRequired()])
+        'first_name', validators=[DataRequired('Please enter your first name.'), Length(min=1, max=20, message='Your first name cannot be greater than 20 characters.')])
     last_name = StringField(
-        'last_name', validators=[DataRequired()])
-    email = StringField('email', validators=[DataRequired(), user_exists])
-    password = StringField('password', validators=[DataRequired()])
+        'last_name', validators=[DataRequired('Please enter your last name.'), Length(min=1, max=20, message='Your last name cannot be greater than 20 characters.')])
+    email = StringField('email', validators=[DataRequired('Please enter your email.'), user_exists])
+    password = StringField('password', validators=[Length(min=8, message='Your password must be at least 8 characters.')])
